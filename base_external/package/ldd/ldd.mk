9eae0fad5659e54e30d0dd176c23cf334ed77731
taha.sami <taha.sami@dreambigsemi.com>
2024-11-13 15:32:41 +0500

    Made changes to S98 script for assignment 7p2

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -1,7 +1,7 @@
 #LDD
 
-LDD_VERSION="75f1c0aa616b29c50050a07543960c6ee723b229"
-LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-tahasami86.git'
+LDD_VERSION=75f1c0aa616b29c50050a07543960c6ee723b229
+LDD_SITE='git@github.com:cu-ecen-aeld/assignment-7-tahasami86.git'
 LDD_SITE_METHOD = git
 LDD_SITE_GIT_SUBMODULES = YES
 
@@ -12,10 +12,10 @@ LDD_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)
 
 define LDD_INSTALL_TARGET_CMDS
 
-	$(INSTALL) -m 0755 $(D)/scull/scull_load	        $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(D)/scull/scull_unload          $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(D)/misc-modules/module_load    $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(D)/misc-modules/module_unload  $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load	        $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload          $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load    $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload  $(TARGET_DIR)/usr/bin
 
 endef
 
